8af6d2e0df9fee21940ebcff3db7d16ca6a6311c
kishore <kishoremakireddi@gmail.com>
2021-11-12 17:12:00 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 /* eslint-disable */
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 function App() {
-  return <div> hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        this is home page
+      </Route>
+      <Route exact path="/starred">
+        this is starred
+      </Route>
+      <Route> 404 page not found</Route>
+    </Switch>
+  );
 }
 
 export default App;
