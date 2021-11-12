8cb4e314b7c3ae0625c51d3ea49e0c140ccdba73
kishore <kishoremakireddi@gmail.com>
2021-11-12 21:51:36 +0530

    created input and fetch api results from tvmaze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,33 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 const Home = () => {
-  return <MainPageLayout>this is home page</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json)
+      .then();
+  };
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) onSearch();
+  };
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        value={input}
+        onKeyDown={onKeyDown}
+      />
+      <button type="button" onClick={onSearch}>
+        search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
